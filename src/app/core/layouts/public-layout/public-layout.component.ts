f80c680033f1e0c5a8adec78fc3d63280b6b47b9
Abdullah-Balasmeh <abdullahbalasmeh.aa@gmail.com>
2026-04-14 03:02:57 +0300

    app footer ui done

--- src/app/core/layouts/public-layout/public-layout.component.ts
+++ src/app/core/layouts/public-layout/public-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LandingHeaderComponent } from "../../../features/landing/presentation/components/landing-header/landing-header.component";
+import { FooterComponent } from "../../../shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-public-layout',
-  imports: [RouterOutlet, LandingHeaderComponent],
+  imports: [RouterOutlet, LandingHeaderComponent, FooterComponent],
   templateUrl: './public-layout.component.html',
   styleUrl: './public-layout.component.css'
 })
